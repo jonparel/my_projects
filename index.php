095b6bdf0b8047c203cd0c43b43b42d5767f636f
Jonathan Parel <jonparel@gmail.com>
2017-06-17 21:32:52 +0900

    This is the initial commitment to my Github.
    This will be the base.

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="description" content="Compilation of awesomely clean humors.">
+        <meta name="keywords" content="jokes, humor, funny, humorous, compilations">
+        <meta name="author" content="Jonathan Parel">
+        <title>Funny, Huh?! | Welcome</title>
+        <link rel="stylesheet" href="./css/main.css">
+    </head>
+    
+    <body>
+        <header>
+            <div class="container">
+                <div id="branding">
+                    <h1><span class="highlight">Funny</span>Collections</h1>
+                </div>
+                
+                <nav>
+                    <ul>
+                        <li class="current"><a href="index.html">Home</a></li>
+					<li><a href="index.php">Home</a></li>
+                        <li><a href="../app/class/view/html/about.php">About</a></li>
+                        <li><a href="../app/class/view/html/contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+        </header>
+        
+        <section id="showcase">
+            <div class="container">
+                <h1>Compilation of hilariousities!"</h1>
+                <p>This is a compilation of clean jokes that are suitable for all ages that you can tell anyone at any moment.
+                    So funny you can crap your pants, so hilarious you cannot breathe, so amusing it will make your day!
+                </p>
+            </div>
+        </section>
+        
+        <section id="newsletter">
+            <div class="container">
+                <h1>Subscribe To Our Jokes</h1>
+                <form action="./app/process.php">
+                    <input type="email" placeholder="Enter eMail...">
+                    <button type="submit" class="button_1">Subscribe</button> 
+                </form>
+            </div>
+        </section>
+        
+        <section id="boxes">
+            <div class="container">
+                <div class="box">
+                    <image src="./images/me1.png">
+                    <h3>Funny</h3>
+                    <p>“If you were half as funny as you think you are, you'd be twice as funny as you really are.” ― H.N. Turteltaub, The Sacred Land
+                    </p>
+                </div>
+                
+                <div class="box">
+                    <image src="./images/me2.png">
+                    <h3>Hilarious</h3>
+                    <p>“Laughter is wine for the soul - laughter soft, or loud and deep, tinged through with seriousness - the hilarious declaration made by man that life is worth living.” ― Seán O'Casey
+                    </p>
+                </div>
+                
+                <div class="box">
+                    <image src="./images/me3.png">
+                    <h3>Amusing</h3>
+                    <p>“You live but once; you might as well be amusing.” ― Coco Chanel
+                    </p>
+                    <p>“I think hiccup cures were really invented for the amusement of the patient's friends.” 
+― Bill Watterson, The Complete Calvin and Hobbes
+                    </p>
+                </div>
+                     
+            </div>
+        </section>
+        
+        <footer>
+            <p><b>FunnyCollections</b> by Jonathan Parel, 2017 | <i>A portfolio of skills that will spare me from starvation</i>.</p>
+        </footer>
+        
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
+
